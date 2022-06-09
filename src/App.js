55c8be2152971f892f9a22bd721657208d0700d6
alam3577 <sajjadalam3577@gmail.com>
2022-06-09 21:41:08 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import Nav from "./components/Layout/Nav";
+import Card from "./components/UI/Card";
+import Login from "./pages/Login";
+import Routers from "./Routers";
 
 function App() {
+  console.log("RERENDER");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <Routers />
+      {/* <Card /> */}
     </div>
   );
 }
